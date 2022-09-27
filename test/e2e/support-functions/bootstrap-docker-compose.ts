6da97fc53a95d7fae6d24b321e626b66b582eac9
Victor Perin <me@victorperin.ninja>
2022-09-27 14:50:56 -0300

    test(e2e): fixing wait strategies

--- test/e2e/support-functions/bootstrap-docker-compose.ts
+++ test/e2e/support-functions/bootstrap-docker-compose.ts
@@ -15,12 +15,12 @@ export const bootstrapDockerCompose = async () => {
     composeFilePath,
     'e2e.docker-compose.yml',
   )
-    .withWaitStrategy('mongo_1', Wait.forHealthCheck())
+    .withWaitStrategy('mongo', Wait.forHealthCheck())
     .withWaitStrategy(
-      'nats_1',
+      'nats',
       waitForText('Listening for client connections on 0.0.0.0:4222'),
     )
-    .withWaitStrategy('scheduler_1', waitForText('App Running'))
+    .withWaitStrategy('scheduler', waitForText('App Running'))
     .withBuild()
     .up();
 
