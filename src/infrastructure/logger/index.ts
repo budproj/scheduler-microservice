3464fbcb7e76bc9207245ae4bc7179a76b0e19cb
Danilo Valente <danilovalente96@gmail.com>
2023-06-22 18:07:53 -0300

    config(logs): improve log formatting (#110)

--- src/infrastructure/logger/index.ts
+++ src/infrastructure/logger/index.ts
@@ -2,11 +2,21 @@ import { createLogger, format, transports } from 'winston';
 
 export const logger = createLogger({
   level: 'info',
-  format: format.simple(),
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.align(),
+    format.simple()
+  ),
   defaultMeta: { service: 'scheduler' },
   transports: [
     new transports.Console({
-      format: format.simple(),
+      format: format.combine(
+        format.colorize(),
+        format.timestamp(),
+        format.align(),
+        format.simple()
+      ),
     }),
   ],
 });
