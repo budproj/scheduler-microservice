07c5c45f77811ae60dc8aae5b943e94dece6e932
Danilo Valente <danilovalente96@gmail.com>
2023-06-22 20:27:02 -0300

    config(logs): add more logs and improve error handling

--- src/jobs/process-routine.ts
+++ src/jobs/process-routine.ts
@@ -19,20 +19,29 @@ export const init = () => {
     async (job: Job<processRoutineData & { queue: string }>): Promise<void> => {
       logger.info('processing routine', job.attrs.data);
 
-      const { queue, ...jobData } = job.attrs.data;
-      const data = jobData;
+      try {
+        const { queue, ...jobData } = job.attrs.data;
+        const data = jobData;
 
-      await publish(queue, data);
+        await publish(queue, data);
+      } catch (err) {
+        logger.error('error processing routine', err);
+        throw err;
+      }
     },
   );
 };
 
-export const scheduleRoutine = (when: string, data: unknown) =>
-  schedule('processRoutine', when, data);
+export const scheduleRoutine = async (when: string, data: unknown) => {
+  logger.info(`scheduling routine with cron "${when}" and data`, data);
+  return await schedule('processRoutine', when,  data);
+}
 
-export const removeRoutine = (data: removeRoutineData) =>
-  cancelJobs<removeRoutineData>({
+export const removeRoutine = async (data: removeRoutineData) => {
+  logger.info(`removing routine with data`, data);
+  return await cancelJobs<removeRoutineData>({
     name: 'processRoutine',
     'data.companyId': data.companyId,
     'data.queue': data.queue,
   });
+}
